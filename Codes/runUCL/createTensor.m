849761dd57f311710645c4f7ffefb073ac19efd2
Spotchi <quentin.laurent93@gmail.com>
2015-05-02 14:11:06 +0200

    Fin analyse + debut influence k

--- Codes/runUCL/createTensor.m
+++ Codes/runUCL/createTensor.m
@@ -1,29 +1,36 @@
-function [mat,A,teachers,NOMAList,carac] = createTensor(option)
+function [mat,A,B,teachers,NOMAList,carac] = createTensor(option)
 [NOMAList,a,~] = runUCL(option);
 [nT,nStud] = size(a);
 nCarac = 2;
 mat = zeros(nT,nStud-1,nCarac);
+B = zeros(size(mat));
 for i = 1 : nT
     for j = 2 : nStud
         nc1 = 0;
         nc2 = 0;
         sum1 = 0;
         sum2 = 0;
+        teacherShare1 = 0;
+        teacherShare2 = 0;
         [~,nEntries] = size(a{i,j});
         for l = 1 : nEntries
             if ~isempty(strfind(a{i,j}{1,l}{1,1},'P'))
                 sum2 = sum2 + a{i,j}{1,l}{1,2};
                 nc2 = nc2 + 1 ;
+                teacherShare1 = teacherShare1 + 1/a{i,j}{1,l}{1,3};
             else
                 sum1 = sum1 + a{i,j}{1,l}{1,2};
                 nc1 = nc1 + 1;
+                teacherShare2 = teacherShare2 + 1/a{i,j}{1,l}{1,3};
             end
         end
         if nc1 > 0
             mat(i,j-1,1) = sum1/nc1;
+            B(i,j-1,1) = teacherShare1/nc1;
         end
         if nc2 > 0
             mat(i,j-1,2) = sum2/nc2;
+            B(i,j-1,2) = teacherShare2/nc2;
         end
     end
 end
