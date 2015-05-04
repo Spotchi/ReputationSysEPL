f6e965bf73e2494f72dc5af818eb7840fe0a8691
Malian <deronmalian@gmail.com>
2015-05-04 05:00:14 +0200

    ...

--- Codes/php/includeMultipleCheatersWithRandomValues.m
+++ Codes/php/includeMultipleCheatersWithRandomValues.m
@@ -7,7 +7,7 @@ ki = 1/(log(2*pi))/10;
 toc;
 
 % # of cheaters
-l = 1000;
+l = 2000;
 
 % # of defavorized hotels
 r = 4;
@@ -24,10 +24,9 @@ for i=1:l
     a = 0;
     b = 5;
     
-    randomCaract = randperm(k, randi([1 k]));
-    cheaters(i, randomHotels, randomCaract) = ((b-a).*rand(1, r, size(randomCaract, 1)) + a);
+    cheaters(i, randomHotels, :) = ((b-a).*rand(1, r, k) + a);
     
-    hotels(i, randomHotels, randomCaract) = 1;
+    hotels(i, randomHotels, :) = 1;
 end
 
 X = [X; cheaters];
@@ -53,7 +52,7 @@ unreliableJudges = getMostUnreliableJudges(W);
 reliableJudges = getMostReliableJudges(W);
 
 %% Histogram
-h = createFigure('Histogram of the weight of users who have voted with one spammer for each hotel');
+h = createFigure('Histogram of the weight of users who have voted with random value on random hotels');
 set(findall(gcf,'type','text'),'FontSize',30,'fontWeight','bold', 'FontName', 'Times New Roman');
 minBar = 1;
 hCheaters = histogram(W(W < minBar), 0:0.1:minBar);
@@ -70,32 +69,3 @@ end
 text(hCheaters.BinEdges(1:end-1) + 0.05, hCheaters.Values/2, num2cell(combined), 'horizontalalignment','center');
 
 diffHist = hCheaters.Values - hBefore.Values;
-
-%% Spammermean
-spammerMean = sum(cheaters)./sum(hotels);
-spammerMean(isnan(spammerMean)) = 0;
-spammerMean = permute(spammerMean, [2 3 1]);
-totalSpammerMean = mean(spammerMean, 2);
-idss = find(totalSpammerMean > 0);
-
-g = createFigure('Score of hotels', 'Sorted hotels', 'Score');
-[sortedNormalMeanWithCheaters, iSorted] = sort(totalNormalMeanWithCheaters);
-h1 = createPlot(g, 1:size(iSorted, 1), sortedNormalMeanWithCheaters);
-%h3 = plot(1:size(totalSpammerMean(idss)), sort(totalSpammerMean(idss)), '*', 'Color', 'blue');
-
-reorderCheaters = cheaters(:, iSorted, :);
-reorderHotels = hotels(:, iSorted, :);
-for i = 1:l
-    for j = 1:m
-        hotelMean = sum(reorderCheaters(i, j, :))./sum(reorderHotels(i, j, :));
-        
-        if hotelMean ~= 0
-            h3 = plot(j, hotelMean, 'x', 'Color', 'blue');
-        end
-    end
-end
-
-meanHotelAfterIter = mean(R, 2);
-[sortedMeanWithCheatersAfterIter, iSortedAfterIter] = sort(meanHotelAfterIter);
-h2 = createPlot(g, 1:size(iSorted, 1), sortedMeanWithCheatersAfterIter);
-legend([h1 h2 h3], {'With spammer', 'After iteration', 'Spam'});
